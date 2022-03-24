76718add817e2cc9c6b42d7de0325a629ef79a58
babymonie <62741287+babymonie@users.noreply.github.com>
2022-03-24 11:24:34 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,6 @@
 
-if game.PlaceId == 4924922222 then 
     local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-    local Window = Library.CreateLib("BRUH Script - Brookhaven", "Serpent")
+    local Window = Library.CreateLib("BRUH Script", "Serpent")
     local Main = Window:NewTab("Main")
     local dababy = Main:NewSection("Local Player")
     local Anim = Window:NewTab("Misc")
@@ -13,6 +12,24 @@ if game.PlaceId == 4924922222 then
     dababy:NewSlider("Jump Power", "Make's Jump Height/Power Higher!", 500, 50, function(s)
         game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
     end)
+    dababy:NewToggle("No Clip","Allows you to walk through walls", function(s)
+        local noclip = true;
+        if s then
+            char = game.Players.LocalPlayer.Character -- Gets your player
+            while true do -- Make sure someone is in game
+            if noclip == true then
+            for _,v in pairs(char:children()) do
+            pcall(function()
+            if v.className == "Part" then
+            v.CanCollide = false
+            end
+            end)
+            end
+            end
+            game:service("RunService").Stepped:wait()
+            end
+        end
+    end)
     dababy:NewToggle("No Fall Damage", "BRUH ITS IN THE NAME!!!!", function(s)
         if s then
             game.Players.LocalPlayer.Character.Humanoid.NoDamage = true
@@ -20,10 +37,6 @@ if game.PlaceId == 4924922222 then
             game.Players.LocalPlayer.Character.Humanoid.NoDamage = false
         end
     end)
-    local playerlist = {} --array of players
-    for i, v in pairs(plrs:GetPlayers()) do
-        table.insert(playerlist, v)
-    end
     dababy:NewDropdown("Player List","a list of players", playerlist, function(currentOption)
         getfenv().target = currentOption
         print(currentOption)
@@ -32,45 +45,10 @@ if game.PlaceId == 4924922222 then
         game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(s, "All")
         getfenv().message = s
     end)
-    dababy:NewButton("Spam Chat", "Spams the chat with the message", function()
-        local message = getfenv().message
-        if message > "" then
-            local time = 0
-            getfenv().time = time
-            --while loop
-            if getfenv().time < 0
-                game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
-                getfenv().time = getfenv().time + 1
-                time = getfenv().time
-            end
-            else
-                game:StarterGui():SetCore("SendNotification", {
-                    Title = "Done";
-                    Text = "Done Spamming";
-                    Duration = 5;
-                })
-            end
-        end
-        else
-            print("Please enter a message")
-        end
-    end)
-    dababy:NewButton("Stop Spamming", "Stops spamming the chat", function()
-        getfenv().time = 0
-    end)
-    --Fling
-    dababy:NewButton("Fling", "Fling the player", function()
-        local target = getfenv().target
-        if target ~= nil then
-        
-        else
-            print("Please select a player")
-        end
-    end)
     local BypassedAudio = Anim:NewSection("Bypassed Audio")
     --enter sound id here
-    BypassedAudio:NewTextBox("Play Sound ID", "Enter the sound ID", function(s)
-        local SoundId = s
+    BypassedAudio:NewTextBox("Play Sound ID", "Enter the sound ID", function(so)
+        local SoundId = so
         local s = Instance.new("Sound")
         s.Name = "RapGod"
         s.SoundId = "http://www.roblox.com/asset/?id=" .. SoundId
@@ -78,22 +56,20 @@ if game.PlaceId == 4924922222 then
         s.Looped = false
         s.archivable = false
         s.Parent = game.Workspace
-        s:Play()
-        --if no sound is playing in the next 1 second, say that the sound is not valid
-        wait(1)
-        if s.IsPlaying == false then
+        if s.TimeLength > 0 then
+            s:Play()
             game.StarterGui:SetCore("SendNotification", {
-                Title = "The given sound id is not valid";
-                Text = "Please enter a valid sound id";,
+                Title = "Sound Bypassed";
+                Text = "Sound ID: " .. SoundId .. " has been played!",
                 Duration = 3,
                 })
         end
-        else
+        if s.TimeLength == 0 then
             game.StarterGui:SetCore("SendNotification", {
-                Title = "Started Sound";
-                Text = "Sound ID: " .. SoundId;
-                Duration = 5;
-            })
+                Title = "Sound Not Bypassed";
+                Text = "Sound ID: " .. SoundId .. " is invalid!",
+                Duration = 3,
+                })
         end
     end)    
     --Stop all sounds
